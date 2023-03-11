5e74b573245472188fbc70dd97a9b9ef11d4e02e
Piyush Gupta <pgtweety1234@gmail.com>
2023-03-11 16:19:57 +0530

    result demo code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
+
+import JSConfetti from 'js-confetti'
+import { useState } from 'react';
+import Results from './results';
+
 import './App.css';
 
+
+
 function App() {
+  const [showComponent, setShowComponent] = useState(false);
+  const [buttonClicked, setButtonClicked] = useState(false);
+
+  function showing() {
+
+    const jsConfetti = new JSConfetti()
+    
+    jsConfetti.addConfetti({
+      emojis: ['✨'],
+      
+      emojiSize: 50,
+      confettiNumber: 100,
+    })
+    
+    setShowComponent(true);
+    setButtonClicked(true);
+    }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       {!buttonClicked && <button onClick={showing}>Click me</button>}
+      {showComponent && <Results />}
     </div>
   );
 }
 
 export default App;
+
---
+++ src/results.js
@@ -0,0 +1,168 @@
+
+
+import './results.css'
+import img1 from './img/pic1.png';
+import img2 from './img/2.png';
+import img3 from './img/3.png';
+import img4 from './img/4.png';
+import img5 from './img/5.png';
+import img6 from './img/6.png';
+import img7 from './img/7.png';
+import img8 from './img/8.png';
+import img9 from './img/9.png';
+import img10 from './img/10.png';
+import img11 from './img/11.png';
+import img12 from './img/12.png';
+import img13 from './img/13.png';
+import img14 from './img/14.png';
+import img15 from './img/15.png';
+import img16 from './img/16.png';
+import img17 from './img/17.png';
+import img18 from './img/18.png';
+import img19 from './img/19.png';
+import img20 from './img/20.png';
+function Results(){
+return(
+  <>
+  
+  <h1>Painting Competition Results</h1>
+    <main>
+    <div id="poster-container">
+      <div className="poster gold">
+      <div className='label'>1st</div>
+        <img src={img1} alt="Poster 1" />
+        
+        <h2>John Doe</h2>
+        <p>First Place</p>
+        <button className="poster-button">View Resume</button>
+      </div>
+      <div className="poster silver">
+      <div className='label'>2nd</div>
+        <img src={img2} alt="Poster 2" />
+        
+        <h2>Jane Smith</h2>
+        <p>Second Place</p>
+        <button className="poster-button">View Resume</button>
+      </div>
+      <div className="poster bronze">
+      <div className='label'>3rd</div>
+        <img src={img3} alt="Poster 3" />
+       
+        <h2>Mike Johnson</h2>
+        <p>Third Place</p>
+        <button className="poster-button">View Resume</button>
+      </div>
+      <div className="poster">
+        <img src={img4} alt="Poster 4" />
+        <h2>Sarah Lee</h2>
+        <p>Top 15</p>
+        <button className="poster-button">View Resume</button>
+      </div>
+      <div className="poster">
+        <img src={img5} alt="Poster 4" />
+        <h2>Sarah Lee</h2>
+        <p>Top 15</p>
+        <button className="poster-button">View Resume</button>
+      </div>
+      <div className="poster">
+        <img src={img6} alt="Poster 4" />
+        <h2>Sarah Lee</h2>
+        <p>Top 15</p>
+        <button className="poster-button">View Resume</button>
+      </div>
+      <div className="poster">
+        <img src={img7} alt="Poster 4" />
+        <h2>Sarah Lee</h2>
+        <p>Top 15</p>
+        <button className="poster-button">View Resume</button>
+      </div>
+      <div className="poster">
+        <img src={img8}alt="Poster 4" />
+        <h2>Sarah Lee</h2>
+        <p>Top 15</p>
+        <button className="poster-button">View Resume</button>
+      </div>
+      <div className="poster">
+        <img src={img9} alt="Poster 4" />
+        <h2>Sarah Lee</h2>
+        <p>Top 15</p>
+        <button className="poster-button">View Resume</button>
+      </div>
+      <div className="poster">
+        <img src={img10} alt="Poster 4" />
+        <h2>Sarah Lee</h2>
+        <p>Top 15</p>
+        <button className="poster-button">View Resume</button>
+      </div>
+      <div className="poster">
+        <img src={img11} alt="Poster 4" />
+        <h2>Sarah Lee</h2>
+        <p>Top 15</p>
+        <button className="poster-button">View Resume</button>
+      </div>
+      <div className="poster">
+        <img src={img12} alt="Poster 4" />
+        <h2>Sarah Lee</h2>
+        <p>Top 15</p>
+        <button className="poster-button">View Resume</button>
+      </div>
+      <div className="poster">
+        <img src={img13} alt="Poster 4" />
+        <h2>Sarah Lee</h2>
+        <p>Top 15</p>
+        <button className="poster-button">View Resume</button>
+      </div>
+      <div className="poster">
+        <img src={img14} alt="Poster 4" />
+        <h2>Sarah Lee</h2>
+        <p>Top 15</p>
+        <button className="poster-button">View Resume</button>
+      </div>
+      <div className="poster">
+        <img src={img15} alt="Poster 4" />
+        <h2>Sarah Lee</h2>
+        <p>Top 15</p>
+        <button className="poster-button">View Resume</button>
+      </div>
+      <div className="poster">
+        <img src={img16}alt="Poster 4" />
+        <h2>Sarah Lee</h2>
+        <p>Top 15</p>
+        <button className="poster-button">View Resume</button>
+      </div>
+      <div className="poster">
+        <img src={img17} alt="Poster 4" />
+        <h2>Sarah Lee</h2>
+        <p>Top 15</p>
+        <button className="poster-button">View Resume</button>
+      </div>
+      <div className="poster">
+        <img src={img18} alt="Poster 4" />
+        <h2>Sarah Lee</h2>
+        <p>Top 15</p>
+        <button className="poster-button">View Resume</button>
+      </div>
+      <div className="poster">
+        <img src={img19} alt="Poster 4" />
+        <h2>Sarah Lee</h2>
+        <p>Top 15</p>
+        <button className="poster-button">View Resume</button>
+      </div>
+      <div className="poster">
+        <img src={img20} alt="Poster 4" />
+        <h2>Sarah Lee</h2>
+        <p>Top 15</p>
+        <button className="poster-button">View Resume</button>
+      </div>
+ 
+    </div>
+   
+    
+</main>
+  </>
+)
+}
+
+export default Results;
+
+
